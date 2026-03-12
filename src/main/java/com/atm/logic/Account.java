0b9882653e8691a2d36397105f451567b9107c7d
q <MatheenatSong@gmail.com>
2026-03-12 18:37:57 +0700

    implement account class

--- src/main/java/com/atm/logic/Account.java
+++ src/main/java/com/atm/logic/Account.java
@@ -6,6 +6,36 @@ public class Account {
 	private double balance;
 	
 	public Account(String id, String pin, double balance) {
-		
+		this.id = id;
+		this.pin = pin;
+		this.balance = Math.max(0, balance);
+	}
+	
+	public double getBalance() {
+		return this.balance;
+	}
+	
+	public boolean validatePin(String inputPin) {
+		return this.pin.equals(inputPin);
+	}
+	
+	public boolean withdraw(double amount) {
+		if(amount > 0 && amount <= this.balance) {
+			this.balance -= amount;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean deposit(double amount) {
+		if(amount > 0) {
+			this.balance += amount;
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
